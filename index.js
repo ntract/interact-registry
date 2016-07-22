28cb49f208a30a69a636949c3557ae1b459ba0f2
Oliver Foster <oliver.foster@kineo.com>
2016-07-22 11:31:03 +0100

    Update

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-global.express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
-global.upload = multer(); // for parsing multipart/form-data
-
-global.app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.raw({
-	type: "application/octet-stream",
-	limit: 2097152
-}));
-
-
 events.once("modules:loaded", function() {
-
 	if (cli.config.commands[0] === "help") return;
+
+
+	global.express = require('express');
+	var bodyParser = require('body-parser');
+	var multer = require('multer'); // v1.0.5
+	global.upload = multer(); // for parsing multipart/form-data
+
+	global.app = express();
+
+	app.set('port', (process.env.PORT || 5000));
+
+	app.use(bodyParser.json()); // for parsing application/json
+	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+	app.use(bodyParser.raw({
+		type: "application/octet-stream",
+		limit: 2097152
+	}));
 
 	console.log("Starting server...");
 
